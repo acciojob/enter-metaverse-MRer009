75bfcae0bc02b2a96b4401c16313b048b73152d7
MRer009 <133023962+MRer009@users.noreply.github.com>
2023-09-19 17:33:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your JS code here. If required.
+const getBtn = document.getElementById("enterbtn");
+const pera = document.getElementById("status");
+getBtn.addEventListener("click" , () => {
+	pera.innerHTML = "<h1>Entered Metaverse</h1>";
+})
